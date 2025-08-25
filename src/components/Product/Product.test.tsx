d20984f89bc33e342f51c2f8962342ffa042ddc1
Anish Saseendran Nirmalakumari(UST <u14605@ust.com>
2025-08-25 12:22:33 +0530

    syntax updated

--- src/components/Product/Product.test.tsx
+++ src/components/Product/Product.test.tsx
@@ -11,8 +11,8 @@ describe('Product page with add to cart', () => {
                 status: 200,
             })
         );
-         const getItemSpy = vi.spyOn(localStorage, 'getItem');
-    getItemSpy.mockReturnValueOnce(JSON.stringify({"id":1,"title":"Essence Mascara Lash Princess","description":"The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.","category":"beauty","price":9.99,"rating":2.56}));
+     const getItemSpy = vi.spyOn(localStorage, 'getItem');
+     getItemSpy.mockReturnValueOnce(JSON.stringify({"id":1,"title":"Essence Mascara Lash Princess","description":"The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.","category":"beauty","price":9.99,"rating":2.56}));
     });
   it('renders the Product component with product cards', async() => {
     render(<Product />);
